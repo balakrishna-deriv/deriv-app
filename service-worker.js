4115e489796526b93da5a8c75e0ab15e1735c8e7
Bala <bala.krishna@regentmarkets.com>
2020-03-13 18:01:53 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a05be376750676332934135e1239068.js"
+  "/deriv-app/precache-manifest.14959e3b2f4376df4fb5a7bb9701e0ba.js"
 );
 
 workbox.core.skipWaiting();
