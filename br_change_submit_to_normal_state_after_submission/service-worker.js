d3f193c2917eb3277339ea5c7c7bfe09dd318da3
Bala <bala.krishna@regentmarkets.com>
2020-05-14 11:02:47 +0800

    Updates

--- br_change_submit_to_normal_state_after_submission/service-worker.js
+++ br_change_submit_to_normal_state_after_submission/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_change_submit_to_normal_state_after_submission/precache-manifest.68184c7736eee76b03febf53a811f1e0.js"
+  "/br_change_submit_to_normal_state_after_submission/precache-manifest.c79b2d18c3a41fed9443c5b92629478c.js"
 );
 
 workbox.core.skipWaiting();
